062cd8226a04a49733eb4ae9411180d5e43af1da
Jason Hindulak <jason.hindulak@hyland.com>
2021-01-11 15:32:43 -0500

    testing validation

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,11 @@
 
 echo "Here are all the environment variables set:"
 env
+
+# Input validation
+case $INPUT_ACTION in
+  (push|pull|repository) ;;
+  (*) printf >&2 'Error: Invalid action, must be one of [push,pull,repository].'; exit 1 ;;
+fi
+
+echo "Successfully parsed action input: $INPUT_ACTION"
